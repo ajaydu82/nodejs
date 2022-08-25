daa1ef55cc920335704e8741b601e0ef03e7fc47
ajay <ajaydu82@gmail.com>
2022-08-25 10:46:45 +0530

    fifth

--- config/db.js
+++ config/db.js
@@ -1,6 +1,10 @@
+const { ServerApiVersion } = require('mongodb');
+
 require('dotenv').config({path:"../.env"});
 const MongoClient = require('mongodb').MongoClient;
 module.exports  = async function getDb(){
-    const db =  await MongoClient.connect(process.env.MONGO_URI);
+    const db =  await MongoClient.connect(process.env.MONGO_URI,{
+        useNewUrlParser:true,useUnifiedTopology:true,serverapi:ServerApiVersion.v1
+    });
     return db.db(process.env.DB_NAME); 
 }
